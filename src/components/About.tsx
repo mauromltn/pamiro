59604a320873652992e3e9e0486183f62fad02fb
mauromltn <mauromtne@icloud.com>
2025-08-18 00:20:57 +0200

    style: Add responsiveness to About component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -19,20 +19,20 @@ const features = [
 
 export default function About() {
    return (
-      <section id='about' className='h-screen grid grid-cols-3 gap-4 px-12 py-14'>
-         <h2 className='font-gambarino text-5xl'>Passion for Quality, Love for Meat</h2>
+      <section id='about' className='min-h-screen grid grid-cols-1 sm:grid-cols-3 gap-4 px-6 sm:px-12 py-14'>
+         <h2 className='font-gambarino text-4xl sm:text-5xl mb-4 sm:mb-0'>Passion for Quality, Love for Meat</h2>
 
-         <section className='col-span-2 content-end ml-30'>
-            <p className='text-[17px] mb-12'>
+         <section className='sm:col-span-2 sm:content-end sm:ml-30'>
+            <p className='text-[15px] sm:text-[17px] mb-8 sm:mb-12'>
                At Deliyumety, we believe that great meals start with the finest ingredients. That&apos;s why we source only the best
                cuts from trusted suppliers. Our chefs are experts in crafting mouthwatering dishes that celebrate the art of
                cooking meat. Whether you crave a tender steak or a hearty BBQ, we ensure every bite is a feast for your senses.
             </p>
-            <section className='grid grid-cols-2 justify-between gap-8 [&>div>h3]:font-gambarino [&>div>h3]:text-3xl [&>div>p]:text-[17px]'>
+            <section className='grid grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-8 [&>div>h3]:font-gambarino [&>div>h3]:text-2xl sm:[&>div>h3]:text-3xl [&>div>p]:text-[15px] sm:[&>div>p]:text-[17px]'>
                {features.map((feature, index) => (
-                  <div key={index} className='w-fit'>
+                  <div key={index} className='w-full sm:w-fit'>
                      <h3>{feature.title}</h3>
-                     <div className='relative w-90 h-90 my-2'>
+                     <div className='relative w-full h-60 sm:w-90 sm:h-90 my-2'>
                         <Image
                            src={feature.image}
                            alt={feature.alt}
