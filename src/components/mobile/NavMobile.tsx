a1ca75d388fa53d0091df97a35e622c2cd6cb0dc
mauromltn <mauromtne@icloud.com>
2025-08-17 23:21:13 +0200

    feat: Add NavMobile component for mobile responsiveness

---
+++ src/components/mobile/NavMobile.tsx
@@ -0,0 +1,49 @@
+'use client';
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'motion/react';
+
+export default function NavMobile() {
+   const [isOpen, setIsOpen] = useState(false);
+
+   return (
+      <>
+         <button onClick={() => setIsOpen(true)} className='block sm:hidden'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" color="#F8EDE0" fill="none">
+               <path d="M4 8.5L20 8.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+               <path d="M4 15.5L20 15.5" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path>
+            </svg>
+         </button>
+
+         <AnimatePresence>
+            {isOpen && (
+               <motion.div
+                  initial={{ y: "-100%" }}
+                  animate={{ 
+                     y: 0,
+                     transition: { duration: 0.2, ease: "backIn" }
+                  }}
+                  exit={{ 
+                     y: "-100%",
+                     transition: { duration: 0.2, ease: "backOut" }
+                  }}
+                  className='absolute inset-0 bg-[var(--secondary)]'
+               >
+                  <button onClick={() => setIsOpen(false)} className='absolute top-5 right-5 z-20'>
+                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="32" height="32" color="#F8EDE0" fill="none">
+                        <path d="M18 6L6.00081 17.9992M17.9992 18L6 6.00085" stroke="#F8EDE0" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+                     </svg>
+                  </button>
+                  <nav className='font-gambarino text-4xl h-full flex justify-center items-center'>
+                     <ul className='text-[var(--primary)] space-y-6'>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#footer">Contact</a></li>
+                        <li><a href="/reservation">Reservation</a></li>
+                     </ul>
+                  </nav>
+               </motion.div>
+            )}
+         </AnimatePresence>
+      </>
+   )
+}
--- src/components/ui/Nav.tsx
+++ src/components/ui/Nav.tsx
@@ -1,18 +1,22 @@
+import NavMobile from "../mobile/NavMobile";
 import Button from "./Button";
 
 export default function Nav() {
    return (
-      <header className='absolute w-full flex justify-between items-center px-12 pt-8 z-10'>
-          <span className="text-3xl font-boska font-bold text-[var(--primary)] tracking-wide">PAMIRÒ</span>
-
-         <nav className='flex justify-end items-center gap-7'>
-            <ul className='flex gap-7 text-[var(--primary)] *:hover:text-[var(--primary-hover)]'>
+      <header className='absolute w-full h-full px-5 sm:px-12 pt-5 sm:pt-8 z-10'>
+         <div className='flex justify-between items-center'>
+            <span className="text-3xl font-boska font-bold text-[var(--primary)] tracking-wide">PAMIRÒ</span>
+            <nav className='hidden sm:flex justify-end items-center gap-7'>
+               <ul className='flex gap-7 text-[var(--primary)] *:hover:text-[var(--primary-hover)]'>
                   <li><a href="/menu">Menu</a></li>
                   <li><a href="#about">About</a></li>
                   <li><a href="#footer">Contact</a></li>
-            </ul>
-            <Button redirectTo='/reservation'>Make a Reservation</Button>
-         </nav>
+               </ul>
+               <Button redirectTo='/reservation'>Make a Reservation</Button>
+            </nav>
+
+            <NavMobile />
+         </div>
       </header>
    )
 }
